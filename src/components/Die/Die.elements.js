64605af4a72ed1aa7ee64bbd1fd87da141d48fd1
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-06-14 10:10:50 -0700

    Update Die.elements.js

--- src/components/Die/Die.elements.js
+++ src/components/Die/Die.elements.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 /*----------- Die Component -----------*/
 export const DieFace = styled.div`
-  height: 70px;
-  width: 70px;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
   border-radius: 5px;
   cursor: pointer;
+  width: 100%;
+  padding-bottom: 100%;
 `;
