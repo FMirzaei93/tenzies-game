51682790bf7f0dff85ad7de96ad87703f05f3f42
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-06-14 14:18:03 -0700

    Create devices.js

---
+++ src/Helper/devices.js
@@ -0,0 +1,24 @@
+const sizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+const devices = {
+  // mobileS: `(min-width: ${sizes.mobileS})`,
+  // mobileM: `(min-width: ${sizes.mobileM})`,
+  // mobileL: `(min-width: ${sizes.mobileL})`,
+  // tablet: `(min-width: ${sizes.tablet})`,
+  // laptop: `(min-width: ${sizes.laptop})`,
+  // laptopL: `(min-width: ${sizes.laptopL})`,
+  // desktop: `(min-width: ${sizes.desktop})`,
+  mobileL: `max-width: ${sizes.mobileL}`,
+  mobileM: `max-width: ${sizes.mobileM}`,
+  mobileS: `max-width: ${sizes.mobileS}`,
+};
+
+export default devices;
