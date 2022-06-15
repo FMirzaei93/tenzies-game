68025ee0397d334991f9fef666f1a1556bfa9282
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-06-15 15:25:02 -0700

    Update Die.elements.js

--- src/components/Die/Die.elements.js
+++ src/components/Die/Die.elements.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 /*----------- Die Component -----------*/
-export const DieFace = styled.div`
+export const DieFaceDiv = styled.div`
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
   border-radius: 5px;
   cursor: pointer;
