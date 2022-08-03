f9c023e4b13c3055a043f5572a49efe7bb291e80
Bence Fekete <fekete965@gmail.com>
2022-08-03 14:42:39 +0100

    Step 7

--- src/Helper/hooks/hookBreakdown.js
+++ src/Helper/hooks/hookBreakdown.js
@@ -131,3 +131,39 @@ export const useLocalStorage6 = (
   return React.useMemo(() => [data, setData], [data]);
 };
 
+// Step 7: Guard agains malformed localStorage data
+export const useLocalStorage7 = (
+  key,
+  initialValue,
+  options = defaultOptions
+) => {
+  const { serialise, deserialise } = options;
+  const [data, setData] = React.useState(() => {
+    const storedValue = localStorage.getItem(key);
+
+    try {
+      if (storedValue !== null) {
+        return deserialise(storedValue);
+      }
+    } catch {
+      return typeof initialValue === "function" ? initialValue() : initialValue;
+    }
+
+    return typeof initialValue === "function" ? initialValue() : initialValue;
+  });
+  const keyRef = React.useRef(key);
+
+  React.useEffect(() => {
+    const prevKey = keyRef.current;
+
+    if (prevKey !== key) {
+      localStorage.removeItem(prevKey);
+    }
+    keyRef.current = key;
+    localStorage.setItem(key, serialise(data));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data, key]);
+
+  return React.useMemo(() => [data, setData], [data]);
+};
+
