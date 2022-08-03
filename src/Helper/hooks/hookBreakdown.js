41cad75b50b378dd81840321eb2907aec7f3c401
Bence Fekete <fekete965@gmail.com>
2022-08-03 14:42:01 +0100

    Step 2

--- src/Helper/hooks/hookBreakdown.js
+++ src/Helper/hooks/hookBreakdown.js
@@ -6,3 +6,16 @@ export const useLocalStorage1 = (initialValue) => {
 
   return React.useMemo(() => [data, setData], [data]);
 };
+
+// Step 2: Save the data to the localStorage on every value change
+export const useLocalStorage2 = (key, initialValue) => {
+  const [data, setData] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(data));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return React.useMemo(() => [data, setData], [data]);
+};
+
